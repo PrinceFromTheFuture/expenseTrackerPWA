6252b64b8dd5dc093bab2de387fde9ef30dde6cb
Amir Waisblay <amirwais.projects@gmail.com>
2024-09-30 21:22:26 +0300

    fd

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -17,6 +17,7 @@ configDotenv();
 const server = express();
 const data = 4;
 console.log(data);
+console.log(data);
 //server.use(cors({ origin: "http://localhost:5173" }));
 //server.use(cors({ origin: process.env.FRONTENDURL }));
 server.use(cors({
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,8 @@ configDotenv();
 const server = express();
 const data = 4
 console.log(data)
+console.log(data)
+
 //server.use(cors({ origin: "http://localhost:5173" }));
 //server.use(cors({ origin: process.env.FRONTENDURL }));
 

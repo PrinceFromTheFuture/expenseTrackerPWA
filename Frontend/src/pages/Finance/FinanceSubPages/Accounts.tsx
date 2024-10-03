efaefc7a97a77b4dd4f539cdb66ee5aa2673c64e
Amir <amirwais.projects@gmail.com>
2024-10-03 15:45:50 +0300

    fsd

--- Frontend/src/pages/Finance/FinanceSubPages/Accounts.tsx
+++ Frontend/src/pages/Finance/FinanceSubPages/Accounts.tsx
@@ -20,7 +20,7 @@ const Accounts = () => {
   ];
   const [openAccount, setOpenAccount] = useState<string | null>(null);
   return (
-    <div className=" w-full mt-4">
+    <div className=" w-full mt-4 min-h-full">
       <div className=" mb-8">
         {" "}
         <div className=" w-full flex justify-end">
@@ -30,110 +30,12 @@ const Accounts = () => {
         </div>
         <div className=" flex justify-center flex-col items-center mt-2">
           {" "}
-          <div className=" text-secondary font-semibold">current blanace</div>
+          <div className=" text-container font-semibold">current blanace</div>
           <div className=" text-4xl text-dark font-extrabold mb-2">{formatAmountInAgorot(3243434 || 0, true)}</div>
           <div className=" bg-success/10 rounded-lg p-1 px-4 text-xs   text-left font-semibold text-success">1234.34 (124.24%)</div>
         </div>
       </div>
       <div className=" text-xl font-semibold mb-2 text-dark">Accoutns</div>
-      {fakeAccounts.map((account) => {
-        return (
-          <div
-            key={account.id}
-            onClick={() => setOpenAccount(openAccount === account.id ? null : account.id)}
-            className=" w-full p-4 py-6 bg-container mb-4 rounded-2xl"
-          >
-            <div className="  flex justify-between items-center ">
-              <div className=" flex justify-start gap-4 items-center">
-                <img src={account.iconURL} className=" w-6 " />
-                <div className="text-base text-secondary font-semibold">{account.name}</div>
-              </div>
-              <motion.img
-                transition={generalTransition}
-                animate={{ rotate: account.id === openAccount ? "180deg" : "0deg" }}
-                src={caret_secondary}
-                className=" w-4"
-              />
-            </div>
-            <AnimatePresence>
-              {openAccount === account.id && (
-                <motion.div
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: "auto", opacity: 1 }}
-                  transition={generalTransition}
-                  exit={{ height: 0, opacity: 0 }}
-                  className=" mt-2"
-                >
-                  <div className=" flex justify-between items-end">
-                    <div>
-                      {" "}
-                      <div className="text-dark font-extrabold text-2xl">{formatAmountInAgorot(account.balanceInAgorot, true)}</div>
-                      <div className="  bg-success/10 rounded-lg p-1 px-4 text-xs   text-left font-semibold mt-1   text-success">
-                        1234.34 (124.24%)
-                      </div>
-                    </div>
-                    <div className=" flex">
-                      <div className="  mr-4 bg-transparent outline outline-secondary text-secondary rounded-lg p-1 px-4 text-xs   text-left font-semibold mt-1  ">
-                        4 Links
-                      </div>
-                      <img src={ellipsis_secondary} className=" h-6" alt="" />
-                    </div>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
-        );
-      })}
-      {fakeAccounts.map((account) => {
-        return (
-          <div
-            key={account.id}
-            onClick={() => setOpenAccount(openAccount === account.id ? null : account.id)}
-            className=" w-full p-4 py-6 bg-container mb-4 rounded-2xl"
-          >
-            <div className="  flex justify-between items-center ">
-              <div className=" flex justify-start gap-4 items-center">
-                <img src={account.iconURL} className=" w-6 " />
-                <div className="text-base text-secondary font-semibold">{account.name}</div>
-              </div>
-              <motion.img
-                transition={generalTransition}
-                animate={{ rotate: account.id === openAccount ? "180deg" : "0deg" }}
-                src={caret_secondary}
-                className=" w-4"
-              />
-            </div>
-            <AnimatePresence>
-              {openAccount === account.id && (
-                <motion.div
-                  initial={{ height: 0, opacity: 0 }}
-                  animate={{ height: "auto", opacity: 1 }}
-                  transition={generalTransition}
-                  exit={{ height: 0, opacity: 0 }}
-                  className=" mt-2"
-                >
-                  <div className=" flex justify-between items-end">
-                    <div>
-                      {" "}
-                      <div className="text-dark font-extrabold text-2xl">{formatAmountInAgorot(account.balanceInAgorot, true)}</div>
-                      <div className="  bg-success/10 rounded-lg p-1 px-4 text-xs   text-left font-semibold mt-1   text-success">
-                        1234.34 (124.24%)
-                      </div>
-                    </div>
-                    <div className=" flex">
-                      <div className="  mr-4 bg-transparent outline outline-secondary text-secondary rounded-lg p-1 px-4 text-xs   text-left font-semibold mt-1  ">
-                        4 Links
-                      </div>
-                      <img src={ellipsis_secondary} className=" h-6" alt="" />
-                    </div>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </div>
-        );
-      })}
       {fakeAccounts.map((account) => {
         return (
           <div

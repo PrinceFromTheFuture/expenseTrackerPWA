6f86f125f52dbe9bc930c11739b8598a104fd58e
Amir Waisblay <amirwais.projects@gmail.com>
2024-11-03 15:38:35 +0200

    fd

--- Frontend/src/pages/Statistics.tsx
+++ Frontend/src/pages/Statistics.tsx
@@ -7,37 +7,74 @@ import {
   getBudgetsSpendingsByMonthsStatusSelector,
   getBudgetsSpendingsByMonthsThunk,
 } from "@/redux/budgetsSlice";
+import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
+import caret_secondary from "@/assets/caret_secondary.svg";
 
 const Statistics = () => {
   const budgetsSpendingsByMonths = useAppSelector(getBudgetsSpendingsByMonthsSelector);
   const budgetsSpendingsByMonthsStatus = useAppSelector(getBudgetsSpendingsByMonthsStatusSelector);
-  const [budgetsSpendingsByMonth, setBudgetsSpendingsByMonth] = useState(budgetsSpendingsByMonths[0]);
+  const [budgetsSpendingsByMonth, setBudgetsSpendingsByMonth] = useState<
+    (typeof budgetsSpendingsByMonths)[0]
+  >(budgetsSpendingsByMonths[0]);
   const allBudets = useAppSelector(allBugdetsSelctor);
+  const [selectedMonth, setSelectedMonth] = useState(dayjs());
 
   return (
     <div className=" w-full fixed  top-0 bottom-0 left-0 right-0 overflow-y-auto overflow-x-hidden bg-surface py-4">
-      <div className=" mx-4 justify-between flex items-end mb-4">
+      <div className=" mx-4 justify-between flex items-end mb-4 items-center">
         <div className="font-bold text-dark text-lg">Statistics</div>
-        <label htmlFor="month123" className=" p-4 rounded-2xl bg-container">
-          fd
+        <label
+          htmlFor="monthSelector"
+          className=" items-center p-4 rounded-2xl flex gap-2 text-secondary bg-container font-semibold"
+        >
+          {selectedMonth.format("MMM YYYY")}
+          <span>
+            {" "}
+            <img src={caret_secondary} alt="" className=" w-4" />
+          </span>
         </label>
-        <input type="month" name="month123" id="month123" className=" absolute " />
+        <input
+          type="month"
+          value={selectedMonth.format("YYYY-MM")}
+          onChange={(event) => {
+            const date = dayjs(event.target.value, "YYYY-MM");
+            setSelectedMonth(date);
+            setBudgetsSpendingsByMonth(
+              budgetsSpendingsByMonths.find(
+                (spendingsObj) =>
+                  spendingsObj.month === date.get("month") && spendingsObj.year === date.get("year")
+              ) || {
+                month: 2,
+                year: 2,
+                data: [
+                  {
+                    budgetId: "3",
+                    amountInAgorot: 1,
+                  },
+                ],
+              }
+            );
+          }}
+          name="monthSelector"
+          id="monthSelector"
+          className=" absolute invisible "
+        />
       </div>
       <div className=" mx-4  border-container border-2 rounded-2xl flex p-4 justify-center items-center flex-col">
         <div className=" relative">
           <div className=" absolute text-center  bottom-0  left-1/2 -translate-x-1/2">
             <div className="text-secondary mb-1 font-semibold text-base">Budgets view</div>
-            <div className="text-dark font-extrabold text-2xl">
+            <div className="text-dark font-extrabold text-2xl text-se">
               {formatAmountInAgorot(
-                budgetsSpendingsByMonth.data.reduce((acc, obj) => acc + obj.amountInAgorot, 0),
+                budgetsSpendingsByMonth?.data.reduce((acc, obj) => acc + obj.amountInAgorot, 0) || 0,
                 true
               )}
             </div>
           </div>
           {budgetsSpendingsByMonthsStatus === "success" && (
             <CircleGraph
-              data={budgetsSpendingsByMonth.data.map((budgetSpendings) => {
+              data={budgetsSpendingsByMonth?.data.map((budgetSpendings) => {
                 return {
                   amount: budgetSpendings.amountInAgorot / 100,
                   color: `#${allBudets.find((budegt) => budegt.id === budgetSpendings.budgetId)!.color}`,
@@ -55,7 +92,10 @@ const Statistics = () => {
           <div className="  flex flex-wrap justify-center gap-2  z-10 max-w-[60%]">
             {" "}
             {budgetsSpendingsByMonth.data.map((budgetObj) => {
-              const budget = allBudets.find((budget) => budget.id === budgetObj.budgetId)!;
+              const budget = allBudets.find((budget) => budget.id === budgetObj.budgetId);
+              if (!budget) {
+                return;
+              }
               return (
                 <div key={budget.id} className=" mr-2 flex justify-between items-center gap-2">
                   {" "}
